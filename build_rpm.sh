82a0efe2cf70c798c5211a9f495bd98784e53df1
RTS <42754493+ranjithtam@users.noreply.github.com>
2022-05-30 12:41:02 -0500

    Update build_rpm.sh

--- build_rpm.sh
+++ build_rpm.sh
@@ -7,10 +7,9 @@ WORKDIR="/home/builder/rpm"
 VERSION="$1"
 SPECURL="https://raw.githubusercontent.com/ranjithtam/specfile/main/grpcurl.spec"
 GRPURL="https://github.com/fullstorydev/grpcurl/archive/refs/tags/v${VERSION}.tar.gz"
-echo $pwd
-echo $PWD
 $wget ${SPECURL}
 $wget ${GRPURL}
 $rpmbld -ba --define "grpcurlver ${VERSION}" /home/builder/rpm/grpcurl.spec
+echo >> ~/.ssh/known_hosts
 $sshk us201 >> ~/.ssh/known_hosts
 cd /home/builder/rpm/x86_64 && $curl --insecure -u ranjith:must0xLogan -T *.rpm sftp://us201/garage/ranjith/
